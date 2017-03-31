8ef20e363cc46c6f4fa37f0daa16cf5a14a4bb38
Nora <anes344@naver.com>
2017-03-31 13:21:16 +0900

    firstcommit

---
+++ Assets/Script/Option/SceneChangeInOption.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangeInOption : MonoBehaviour {
+
+    public void GoToBack()
+    {
+        // 뒤로 가기
+        // Application.Quit();
+        // Main으로 보내기
+        SceneManager.LoadScene("Main");
+    }
+}
